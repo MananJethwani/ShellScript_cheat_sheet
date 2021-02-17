bd737aa68f2f17117c1a165b1a29cda2930c3dfc
Manan Jethwani <mananjethwani02@gmail.com>
2021-02-17 13:33:47 +0530

    basic scripts

---
+++ redirectOutputToFile.sh
@@ -0,0 +1,5 @@
+# use > to store output in a particular file
+echo "to store in file.txt">file.txt
+# to store input from terminal to a specific file use cat
+cat>file.txt
+# use >> to append instead of rewriting the file
